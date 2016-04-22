1fee9db601d9e21f2231a57e365206ce55384ec2
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2016-04-22 15:21:23 +0200

    [ADD] TcpList KO messages.

--- tests/run.py
+++ tests/run.py
@@ -19,6 +19,24 @@ class TestPerMessages(unittest.TestCase):
         sand_message = etree.parse(message)
         self.assertFalse(self.sand_schema.validate(sand_message))
         print "Test succesful : " + message
+
+class TestMetricsMessages(unittest.TestCase):
+  def setUp(self):
+      with open("../schemas/sand_messages.xsd") as f: 
+        sand_schema_doc = etree.parse(f)
+        self.sand_schema = etree.XMLSchema(sand_schema_doc)
+
+  def test_valid_messages(self):
+      for message in glob.glob("../metrics/*-OK-*.xml"):
+        sand_message = etree.parse(message)
+        self.assertTrue(self.sand_schema.validate(sand_message))
+        print "Test succesful : " + message
+  
+  def test_invalid_messages(self):
+      for message in glob.glob("../metrics/*-KO-*.xml"):
+        sand_message = etree.parse(message)
+        self.assertFalse(self.sand_schema.validate(sand_message))
+        print "Test succesful : " + message
   
 if __name__ == '__main__':
       unittest.main()
