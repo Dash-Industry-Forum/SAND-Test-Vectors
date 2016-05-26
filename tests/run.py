8b4852eb99e3509d4f3960bd17f362453ea029eb
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2016-05-27 00:20:30 +0200

    [ADD] Txt PER and PED tests

--- tests/run.py
+++ tests/run.py
@@ -8,7 +8,7 @@ sys.path.append("..")
 import sand.header
 from sand.xml_message import XMLValidator
 
-class TestPerMessages(unittest.TestCase):
+class TestXmlPerMessages(unittest.TestCase):
     def setUp(self):
         self.validator = XMLValidator()
 
@@ -22,7 +22,7 @@ class TestPerMessages(unittest.TestCase):
             self.assertFalse(self.validator.from_file(message))
             print "Test succesful : " + message
 
-class TestMetricsMessages(unittest.TestCase):
+class TestXmlMetricsMessages(unittest.TestCase):
     def setUp(self):
         self.validator = XMLValidator()
 
@@ -36,39 +36,62 @@ class TestMetricsMessages(unittest.TestCase):
             self.assertFalse(self.validator.from_file(message))
             print "Test succesful : " + message
 
-class TestStatussMessages(unittest.TestCase):
+def check_header(header):
+    match = re.search("([^:]+):(.+)", header)
+    name = match.group(1).strip()
+    value = match.group(2).strip()
+    checker = sand.header.header_name_to_checker.get(name.lower())
+    if checker:
+        checker.check_syntax(value)
+        return checker.errors
+    else:
+        print ("Header name not supported by this " 
+               "version of conformance server|" + name) 
+        
+
+class TestTxtStatussMessages(unittest.TestCase):
     def test_valid_messages(self):
         for message_file in glob.glob("../status/*-OK-*.txt"):
             with open(message_file) as message:
                 for header in message.readlines():
-                    match = re.search("([^:]+):(.+)", header.replace(" ", ""))
-                    name = match.group(1)
-                    value = match.group(2)
-                    checker = sand.header.header_name_to_checker.get(name.lower())
-                    if checker:
-                        checker.check_syntax(value)
-                    else:
-                        print ("Header name not supported by this " 
-                               "version of conformance server.") 
-        
-                    self.assertFalse(checker.errors)
+                    self.assertFalse(check_header(header))
                     print "Test succesful : " + message_file
   
     def test_invalid_messages(self):
         for message_file in glob.glob("../status/*-KO-*.txt"):
             with open(message_file) as message:
                 for header in message.readlines():
-                    match = re.search("([^:]+):(.+)", header.replace(" ", ""))
-                    name = match.group(1)
-                    value = match.group(2)
-                    checker = sand.header.header_name_to_checker.get(name.lower())
-                    if checker:
-                        checker.check_syntax(value)
-                    else:
-                        print ("Header name not supported by this "
-                               "version of conformance server.")
-        
-                    self.assertTrue(checker.errors)
+                    self.assertTrue(check_header(header))
+                    print "Test succesful : " + message_file
+
+class TestTxtPerMessages(unittest.TestCase):
+    def test_valid_messages(self):
+        for message_file in glob.glob("../per/*-OK-*.txt"):
+            with open(message_file) as message:
+                for header in message.readlines():
+                    self.assertFalse(check_header(header))
+                    print "Test succesful : " + message_file
+  
+    def test_invalid_messages(self):
+        for message_file in glob.glob("../per/*-KO-*.txt"):
+            with open(message_file) as message:
+                for header in message.readlines():
+                    self.assertTrue(check_header(header))
+                    print "Test succesful : " + message_file
+
+class TestTxtPedMessages(unittest.TestCase):
+    def test_valid_messages(self):
+        for message_file in glob.glob("../ped/*-OK-*.txt"):
+            with open(message_file) as message:
+                for header in message.readlines():
+                    self.assertFalse(check_header(header))
+                    print "Test succesful : " + message_file
+  
+    def test_invalid_messages(self):
+        for message_file in glob.glob("../ped/*-KO-*.txt"):
+            with open(message_file) as message:
+                for header in message.readlines():
+                    self.assertTrue(check_header(header))
                     print "Test succesful : " + message_file
 
 if __name__ == '__main__':
