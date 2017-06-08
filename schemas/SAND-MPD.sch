8bf46ba69143c3437e4f26b26438e1c7f43ad654
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2017-06-08 16:47:36 +0200

    [FIX] Reporting@value attribute shall contain the value of a sand:Channel@id attribute. MPEG_SAND/pylibsand#10

--- schemas/SAND-MPD.sch
+++ schemas/SAND-MPD.sch
@@ -1,17 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<schema xmlns="http://purl.oclc.org/dsdl/schematron" xmlns:sand="urn:mpeg:dash:schema:sand:2016" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" queryBinding='xslt' schemaVersion='ISO19757-3'>
+<schema xmlns="http://purl.oclc.org/dsdl/schematron" xmlns:dash="urn:mpeg:dash:schema:mpd:2011" xmlns:sand="urn:mpeg:dash:schema:sand:2016" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" queryBinding='xslt' schemaVersion='ISO19757-3'>
+  <ns prefix="dash" uri="urn:mpeg:dash:schema:mpd:2011"/>
   <ns prefix="sand" uri="urn:mpeg:dash:schema:sand:2016"/>
   <ns prefix="xsi" uri="http://www.w3.org/2001/XMLSchema-instance"/>
   <pattern>
     <rule context="sand:Channel">
-      <assert test="(./@schemeIdUri = 'urn:mpeg:dash:sand:channel:websocket:2016' and (starts-with(./@endpoint, 'ws://') or starts-with(./@endpoint, 'wss://'))) or ./@schemeIdUri != 'urn:mpeg:dash:sand:channel:websocket:2016'">
+      <assert test="(@schemeIdUri = 'urn:mpeg:dash:sand:channel:websocket:2016' and (starts-with(@endpoint, 'ws://') or starts-with(@endpoint, 'wss://'))) or @schemeIdUri != 'urn:mpeg:dash:sand:channel:websocket:2016'">
         If the @schemeIdUri is "urn:mpeg:dash:sand:channel:websocket:2016", the @endpoint shall be a WebSocket URI according to RFC 6455.
       </assert>
-      <assert test="(./@schemeIdUri = 'urn:mpeg:dash:sand:channel:http:2016' and (starts-with(./@endpoint, 'http://') or starts-with(./@endpoint, 'https://'))) or ./@schemeIdUri != 'urn:mpeg:dash:sand:channel:http:2016'">
+      <assert test="(@schemeIdUri = 'urn:mpeg:dash:sand:channel:http:2016' and (starts-with(@endpoint, 'http://') or starts-with(@endpoint, 'https://'))) or @schemeIdUri != 'urn:mpeg:dash:sand:channel:http:2016'">
         If the @schemeIdUri is "urn:mpeg:dash:sand:channel:http:2016", the @endpoint shall be a valid HTTP-URL according to RFC 3986.
       </assert>
-      <assert test="(./@schemeIdUri = 'urn:mpeg:dash:sand:channel:header:2016' and count(./@endpoint) = 0) or ./@schemeIdUri != 'urn:mpeg:dash:sand:channel:header:2016'">
+      <assert test="(@schemeIdUri = 'urn:mpeg:dash:sand:channel:header:2016' and count(@endpoint) = 0) or @schemeIdUri != 'urn:mpeg:dash:sand:channel:header:2016'">
         If the @schemeIdUri is "urn:mpeg:dash:sand:channel:http:2016", the @endpoint shall not present.
+      </assert>
+    </rule>
+    <rule context="dash:Reporting">
+        <assert test="(@schemeIdUri != 'urn:mpeg:dash:sand:channel:2016') or (@schemeIdUri = 'urn:mpeg:dash:sand:channel:2016' and @value = //sand:Channel/@id) ">
+        5.H.3 - If SAND Channle is used for reporting, then @value shall contain
+        the value of @id of a SAND Channel present in the MPD.
       </assert>
     </rule>
   </pattern>
--- tests/run.py
+++ tests/run.py
@@ -49,85 +49,114 @@ import logging
 logging.basicConfig(filename='report.log', level=logging.DEBUG, filemode='w')
 
 class TestMpd(unittest.TestCase):
+    """
+    Test the conformance of MPD test vectors.
+    """
     def setUp(self):
-        with open("../schemas/SAND-MPD.xsd") as f:
-            sand_mpd_schema_doc = etree.parse(f)
+        with open("../schemas/SAND-MPD.xsd") as schema:
+            sand_mpd_schema_doc = etree.parse(schema)
             self.sand_mpd_schema = etree.XMLSchema(sand_mpd_schema_doc)
-        with open("../schemas/SAND-MPD.sch") as f:
-            sand_mpd_schmeatron_doc = etree.parse(f)
-            self.sand_mpd_schematron = isoschematron.Schematron(sand_mpd_schmeatron_doc)
+        with open("../schemas/SAND-MPD.sch") as schematron:
+            sand_mpd_schmeatron_doc = etree.parse(schematron)
+            self.sand_mpd_schematron = isoschematron.Schematron(
+                sand_mpd_schmeatron_doc)
 
     def test_valid_mpds(self):
+        """
+        Test that valid MPD test vectors are indeed conformant.
+        """
         for mpd_path in glob.glob("../mpd/*-OK-*.xml"):
             try:
                 mpd = etree.parse(mpd_path)
                 self.assertTrue(
                     self.sand_mpd_schema.validate(mpd)
-                and self.sand_mpd_schematron.validate(mpd))
+                    and self.sand_mpd_schematron.validate(mpd))
                 logging.info("Test succesful : %s", mpd_path)
-            except Exception as e:
+            except Exception as error:
                 logging.error("Test : %s KO", mpd_path)
-                raise type(e)(e.message + " %s" % mpd_path)
+                raise type(error)(error.message + " %s" % mpd_path)
 
     def test_invalid_mpds(self):
+        """
+        Test that invalid MPD test vectors are indeed not conformant.
+        """
         for mpd_path in glob.glob("../mpd/*-KO-*.xml"):
             try:
                 mpd = etree.parse(mpd_path)
                 self.assertFalse(
                     self.sand_mpd_schema.validate(mpd)
-                and self.sand_mpd_schematron.validate(mpd))
+                    and self.sand_mpd_schematron.validate(mpd))
                 logging.info("Test succesful : %s", mpd_path)
-            except Exception as e:
+            except Exception as error:
                 logging.error("Test : %s KO", mpd_path)
-                raise type(e)(e.message + " %s" % mpd_path)
+                raise type(error)(error.message + " %s" % mpd_path)
 
 class TestXmlPerMessages(unittest.TestCase):
+    """
+    Test the conformance of PER message test vectors.
+    """
     def setUp(self):
         self.validator = XMLValidator()
 
     def test_valid_messages(self):
+        """
+        Test that valid PER message test vectors are indeed conformant.
+        """
         for message in glob.glob("../per/*-OK-*.xml"):
             try:
                 logging.info("Test : %s ...", message)
                 self.assertTrue(self.validator.from_file(message))
                 logging.info("Test : %s OK", message)
-            except Exception as e:
+            except Exception as error:
                 logging.error("Test : %s KO", message)
-                raise type(e)(e.message + " %s" % message)
-  
+                raise type(error)(error.message + " %s" % message)
+
     def test_invalid_messages(self):
+        """
+        Test that invalid PER message test vectors are indeed not conformant.
+        """
         for message in glob.glob("../per/*-KO-*.xml"):
             try:
                 logging.info("Test : %s ...", message)
                 self.assertFalse(self.validator.from_file(message))
                 logging.info("Test : %s OK", message)
-            except Exception as e:
+            except Exception as error:
                 logging.error("Test : %s KO", message)
-                raise type(e)(e.message + " %s" % message)
+                raise type(error)(error.message + " %s" % message)
 
 class TestXmlMetricsMessages(unittest.TestCase):
+    """
+    Test the conformance of PER text message test vectors.
+    """
     def setUp(self):
         self.validator = XMLValidator()
 
     def test_valid_messages(self):
+        """
+        Test that valid Metrics message test vectors are indeed conformant.
+        """
         for message in glob.glob("../metrics/*-OK-*.xml"):
             try:
                 logging.info("Test : %s ...", message)
                 self.assertTrue(self.validator.from_file(message))
                 logging.info("Test : %s OK", message)
-            except Exception as e:
+            except Exception as error:
                 logging.error("Test : %s KO", message)
-                raise type(e)(e.message + " %s" % message)
-  
+                raise type(error)(error.message + " %s" % message)
+
     def test_invalid_messages(self):
+        """
+        Test that invalid Metrics message test vectors are indeed not
+        conformant.
+        """
         for message in glob.glob("../metrics/*-KO-*.xml"):
             try:
                 logging.info("Test : %s ...", message)
                 self.assertFalse(self.validator.from_file(message))
                 logging.info("Test : %s OK", message)
-            except Exception as e:
+            except Exception as error:
                 logging.error("Test : %s KO", message)
-                raise type(e)(e.message + " %s" % message)
+                raise type(error)(error.message + " %s" % message)
 
 def is_header_valid(header):
     ret = False
@@ -143,14 +172,20 @@ def is_header_valid(header):
         else:
             logging.error(checker.errors)
     else:
-        logging.warning(("Header %s not supported by this " 
+        logging.warning(("Header %s not supported by this "
                          "version of conformance server"),
-                        name) 
+                        name)
     return ret
-        
+
 
 class TestTxtStatussMessages(unittest.TestCase):
+    """
+    Test the conformance of Status message test vectors.
+    """
     def test_valid_messages(self):
+        """
+        Test that valid Status message test vectors are indeed conformant.
+        """
         for message_file in glob.glob("../status/*-OK-*.txt"):
             with open(message_file) as message:
                 for header in message.readlines():
@@ -158,11 +193,14 @@ class TestTxtStatussMessages(unittest.TestCase):
                         logging.info("Test : %s ...", message_file)
                         self.assertTrue(is_header_valid(header))
                         logging.info("Test : %s OK", message_file)
-                    except Exception as e:
+                    except Exception as error:
                         logging.error("Test : %s KO", message_file)
-                        raise type(e)(e.message + " %s" % message_file)
-  
+                        raise type(error)(error.message + " %s" % message_file)
+
     def test_invalid_messages(self):
+        """
+        Test that invalid Status message test vectors are indeed not conformant.
+        """
         for message_file in glob.glob("../status/*-KO-*.txt"):
             with open(message_file) as message:
                 for header in message.readlines():
@@ -170,12 +208,18 @@ class TestTxtStatussMessages(unittest.TestCase):
                         logging.info("Test : %s ...", message_file)
                         self.assertFalse(is_header_valid(header))
                         logging.info("Test : %s OK", message_file)
-                    except Exception as e:
+                    except Exception as error:
                         logging.error("Test : %s KO", message_file)
-                        raise type(e)(e.message + " %s" % message_file)
+                        raise type(error)(error.message + " %s" % message_file)
 
 class TestTxtPerMessages(unittest.TestCase):
+    """
+    Test the conformance of PER text message test vectors.
+    """
     def test_valid_messages(self):
+        """
+        Test that valid PER text message test vectors are indeed conformant.
+        """
         for message_file in glob.glob("../per/*-OK-*.txt"):
             with open(message_file) as message:
                 for header in message.readlines():
@@ -183,11 +227,15 @@ class TestTxtPerMessages(unittest.TestCase):
                         logging.info("Test : %s ...", message_file)
                         self.assertTrue(is_header_valid(header))
                         logging.info("Test : %s OK", message_file)
-                    except Exception as e:
+                    except Exception as error:
                         logging.error("Test : %s KO", message_file)
-                        raise type(e)(e.message + " %s" % message_file)
-  
+                        raise type(error)(error.message + " %s" % message_file)
+
     def test_invalid_messages(self):
+        """
+        Test that invalid PER text message test vectors are indeed not
+        conformant.
+        """
         for message_file in glob.glob("../per/*-KO-*.txt"):
             with open(message_file) as message:
                 for header in message.readlines():
@@ -195,9 +243,9 @@ class TestTxtPerMessages(unittest.TestCase):
                         logging.info("Test : %s ...", message_file)
                         self.assertFalse(is_header_valid(header))
                         logging.info("Test : %s OK", message_file)
-                    except Exception as e:
+                    except Exception as error:
                         logging.error("Test : %s KO", message_file)
-                        raise type(e)(e.message + " %s" % message_file)
+                        raise type(error)(error.message + " %s" % message_file)
 
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().loadTestsFromModule(__import__(__name__))
